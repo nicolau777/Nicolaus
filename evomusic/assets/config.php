966b11a60c103c2213508e4c03f85598cfac8db9
nicolau777 <135940083+nicolau777@users.noreply.github.com>
2023-06-16 08:27:45 -0300

    Add files via upload

---
+++ evomusic/assets/config.php
@@ -0,0 +1,14 @@
+
+<?php
+$dbHost = 'localhost';
+$dbUsername = 'root';
+$dbPassword = 'nicolaueak';
+$dbName = 'projectMusic';
+
+
+$conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+
+
+?>
+</body>
+</html>
